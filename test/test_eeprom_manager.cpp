c1c358ee1ee00d4bdc75bd97bfaed44536f68ba4
hayasita <hayasita04@gmail.com>
2025-06-30 22:26:56 +0900

    [Update] EepromTest動作するように修正。（テスト内容は仮

--- test/test_eeprom_manager.cpp
+++ test/test_eeprom_manager.cpp
@@ -1,31 +1,43 @@
 #include <gtest/gtest.h>
 #include "../src/EepromManager.h"
-//#include "mock/MockI2CBusManager.h"  // 必要ならモックをインクルード
+#include "mock/DummyI2CBusManager.h"  // 必要ならモックをインクルード
+
+class DummyEepromRawAccess{
+};
 
 // テストフィクスチャ
 class EepromManagerTest : public ::testing::Test {
 protected:
-    EepromManager eepromManager;
-    MockI2CBusManager mockBusManager;
+  EepromManager eepromManager;
+  DummyI2CBusManager mockBusManager;
+  DummyEepromRawAccess mockEepromRawAccess;
 
-    void SetUp() override {
-        // 必要なら初期化
-        eepromManager.begin(mockBusManager);
-    }
+  EepromManagerTest()
+  : eepromManager(&mockBusManager),  // EepromManagerのコンストラクタにモックを渡す
+    mockBusManager(),                // モックのI2CBusManagerを初期化
+    mockEepromRawAccess()           // モックのEepromRawAccessを初期化
+  {
+    // コンストラクタで初期化
+  }
 
-    void TearDown() override {
-        // 必要なら後処理
-    }
+  void SetUp() override {
+    // 必要なら初期化
+    eepromManager.begin();
+  }
+
+  void TearDown() override {
+    // 必要なら後処理
+  }
 };
 
 // サンプルテスト
 TEST_F(EepromManagerTest, WriteAndReadBytes) {
-    uint8_t writeData[4] = {1, 2, 3, 4};
-    uint8_t readData[4] = {0};
+  uint8_t writeData[4] = {1, 2, 3, 4};
+  uint8_t readData[4] = {0};
 
-    EXPECT_TRUE(eepromManager.writeBytes(0, writeData, 4));
-    EXPECT_TRUE(eepromManager.readBytes(0, readData, 4));
-    EXPECT_EQ(0, memcmp(writeData, readData, 4));
+  EXPECT_TRUE(eepromManager.writeBytes(0, writeData, 4));
+  EXPECT_TRUE(eepromManager.readBytes(0, readData, 4));
+  EXPECT_EQ(0, memcmp(writeData, readData, 4));
 }
 
 // 他のテストケースもここに追加
